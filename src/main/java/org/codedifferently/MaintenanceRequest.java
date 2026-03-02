6c6d87e316da9ef7cb0e3b9b7a10d9232a9208e1
Bryant Ferguson <btbf569708@gmail.com>
2026-03-02 00:31:21 -0500

    feat: created maintenance request class

---
+++ src/main/java/org/codedifferently/MaintenanceRequest.java
@@ -0,0 +1,70 @@
+package org.codedifferently;
+
+public class MaintenanceRequest {
+    private String tenantName;
+    private int apartmentNumber;
+    private String issueType;
+    private int severity;
+    private String status;
+
+    public MaintenanceRequest(String tenantName, int apartmentNumber, String issueType, int severity, String status) {
+        this.tenantName = tenantName;
+        this.apartmentNumber = apartmentNumber;
+        this.issueType = issueType;
+        this.severity = severity;
+        this.status = status;
+    }
+
+    public String getTenantName() {
+        return tenantName;
+    }
+
+    public void setTenantName(String tenantName) {
+        this.tenantName = tenantName;
+    }
+
+    public int getApartmentNumber() {
+        return apartmentNumber;
+    }
+
+    public void setApartmentNumber(int apartmentNumber) {
+        this.apartmentNumber = apartmentNumber;
+    }
+
+    public String getIssueType() {
+        return issueType;
+    }
+
+    public void setIssueType(String issueType) {
+        this.issueType = issueType;
+    }
+
+    public int getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(int severity) {
+        if (severity >= 1 && severity <= 5) {
+            this.severity = severity;
+        } else {
+            System.out.println("Invalid severity.");
+        }
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Tenant: " + tenantName +
+                " | Apt: " + apartmentNumber +
+                " | Issue: " + issueType +
+                " | Severity: " + severity +
+                " | Status: " + status;
+    }
+}
