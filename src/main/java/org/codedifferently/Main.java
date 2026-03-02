7312d9612f24f471a5c817d3ea2b99d8620bc260
Derwin Bell <djbell261@gmail.com>
2026-03-02 08:35:21 -0500

    apartment maintaenance request tracker complete

--- src/main/java/org/codedifferently/Main.java
+++ src/main/java/org/codedifferently/Main.java
@@ -2,7 +2,7 @@ package org.codedifferently;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/>
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
