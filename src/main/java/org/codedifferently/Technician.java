2a99a0e1961d65796242e19461a4578a0ff47809
Bryant Ferguson <btbf569708@gmail.com>
2026-03-02 00:45:31 -0500

    feat: created a technician class

---
+++ src/main/java/org/codedifferently/Technician.java
@@ -0,0 +1,17 @@
+package org.codedifferently;
+
+public class Technician {
+    private String type;
+
+    public Technician(String type){
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
